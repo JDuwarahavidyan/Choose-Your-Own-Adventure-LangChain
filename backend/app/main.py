8ca8d56ac160da97bfd7161ceeb327137e14bfd0
JDuwarahavidyan <duwarahavidyan_j_e22@engug.ruh.ac.lk>
2026-03-15 18:46:28 +0530

    Updated Backend for OpenAI Service with Final fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -35,4 +35,4 @@ app.include_router(job.router)
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000)
